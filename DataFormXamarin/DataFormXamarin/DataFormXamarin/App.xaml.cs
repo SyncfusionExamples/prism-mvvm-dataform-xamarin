dd30c657cf1ad8aea39ea713c3e15dd02dbc93fe
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-02-26 16:46:46 +0530

    XAMARIN-33591 - Sample added.

---
+++ DataFormXamarin/DataFormXamarin/DataFormXamarin/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace DataFormXamarin
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("DataFormPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<DataFormPage, DataFormViewModel>();
+        }
+    }
+}
---
+++ DataFormXamarin/DataFormXamarin/DataFormXamarin/Model/ContactsInfo.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace DataFormXamarin
+{
+    public class ContactsInfo : INotifyPropertyChanged
+    {
+        private string firstName;
+        private string middleName;
+        private string lastName;
+        private string contactNo;
+        private string email;
+        private string address;
+        private DateTime? birthDate;
+        private string groupName;
+
+        public ContactsInfo()
+        {
+
+        }
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set
+            {
+                this.firstName = value;
+                this.RaiseOnPropertyChanged("FirstName");
+            }
+        }
+
+        public string MiddleName
+        {
+            get { return this.middleName; }
+            set
+            {
+                this.middleName = value;
+                this.RaiseOnPropertyChanged("MiddleName");
+            }
+        }
+        public string LastName
+        {
+            get { return this.lastName; }
+            set
+            {
+                this.lastName = value;
+                this.RaiseOnPropertyChanged("LastName");
+            }
+        }
+
+        public string ContactNumber
+        {
+            get { return contactNo; }
+            set
+            {
+                this.contactNo = value;
+                this.RaiseOnPropertyChanged("ContactNumber");
+            }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                this.RaiseOnPropertyChanged("Email");
+            }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set
+            {
+                address = value;
+                this.RaiseOnPropertyChanged("Address");
+            }
+        }
+
+        public DateTime? BirthDate
+        {
+            get { return birthDate; }
+            set
+            {
+                birthDate = value;
+                this.RaiseOnPropertyChanged("BirthDate");
+            }
+        }
+
+        public string GroupName
+        {
+            get { return groupName; }
+            set
+            {
+                groupName = value;
+                this.RaiseOnPropertyChanged("GroupName");
+            }
+        }
+
+
+        /// <summary>
+        /// Occurs when property changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Invoke method when property changed.
+        /// </summary>
+        /// <param name="propertyName">property name</param>
+        private void RaiseOnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
